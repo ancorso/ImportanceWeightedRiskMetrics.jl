c065b167a1ef1dccddc021ac4a01f1dfa07c810a
Shubh Gupta <shubhg1996@gmail.com>
2022-01-19 00:35:53 -0800

    flipped CDF estimation

--- src/online_cdf.jl
+++ src/online_cdf.jl
@@ -2,8 +2,8 @@
 Running cdf estimator
 """
 mutable struct RunningCDFEstimator
-    Xs::Vector{Float64}
-    partial_Ws::Vector{Float64}
+    Xs::Vector{Float64} # stored in increasing order for easy sorting
+    partial_Ws::Vector{Float64} # stored in reverse order cumsum for easy sorting (first entry corresponds to last Xs)
     last_i::Int
 end
 
@@ -12,7 +12,7 @@ RunningCDFEstimator() = RunningCDFEstimator([], [], 0)
 function RunningCDFEstimator(X::Vector{Float64}, W::Vector{Float64})
     # TODO: combine weights of duplicate entries in cost for efficiency
     perm = sortperm(X, rev=true)
-    Xs = X[perm]
+    Xs = reverse(X[perm])
     Ws = W[perm]
     partial_Ws = cumsum(Ws)
     n = length(X)
@@ -24,11 +24,11 @@ end
 CDF function
 """
 function cdf(est::RunningCDFEstimator, x)
-    idx = searchsortedfirst(est.Xs, x)
-    if idx > est.last_i
+    idx = searchsortedlast(est.Xs, x)
+    if idx < 1
         return 0.0
     else
-        return 1.0 - est.partial_Ws[idx] / est.last_i
+        return 1.0 - est.partial_Ws[end - idx + 1] / est.last_i
     end
 end
 
@@ -40,7 +40,7 @@ function quantile(est::RunningCDFEstimator, α::Float64)
     if idx < 1
         idx = 1
     end
-    return est.Xs[idx]
+    return est.Xs[end - idx + 1]
 end
 
 """
@@ -52,20 +52,21 @@ function update!(est::RunningCDFEstimator, x, w)
         push!(est.partial_Ws, w)
     elseif x < first(est.Xs)
         pushfirst!(est.Xs, x)
-        pushfirst!(est.partial_Ws, 0.0)
-        est.partial_Ws = est.partial_Ws .+ w
+        push!(est.partial_Ws, last(est.partial_Ws) + w)
     elseif x > last(est.Xs)
         push!(est.Xs, x)
-        push!(est.partial_Ws, last(est.partial_Ws) + w)
+        pushfirst!(est.partial_Ws, 0.0)
+        est.partial_Ws = est.partial_Ws .+ w
     else
         new_idx = searchsortedlast(est.Xs, x) + 1
         if est.Xs[new_idx-1] == x   # special handle for efficient handling of exactly equal values
-            new_idx = new_idx - 1
+            w_idx =  length(est.partial_Ws) - new_idx + 2
         else
             splice!(est.Xs, new_idx:new_idx-1, [x])
-            splice!(est.partial_Ws, new_idx:new_idx-1, [est.partial_Ws[new_idx-1]])
+            w_idx = length(est.partial_Ws) - new_idx + 1
+            splice!(est.partial_Ws, w_idx+1:w_idx, [est.partial_Ws[new_idx-1]])
         end
-        est.partial_Ws[new_idx:end] = est.partial_Ws[new_idx:end] .+ w
+        est.partial_Ws[w_idx:end] = est.partial_Ws[w_idx:end] .+ w
     end
     est.last_i += 1
 end
