df8aae9ee647035180c8904c286e0dd55983d26a
Shubh Gupta <shubhg1996@gmail.com>
2022-01-25 11:51:50 -0800

    fixed tail-cost calculation

--- src/online_cdf.jl
+++ src/online_cdf.jl
@@ -4,7 +4,7 @@ Running cdf estimator
 mutable struct RunningCDFEstimator
     Xs::Vector{Float64} # stored in increasing order for easy sorting
     partial_Ws::Vector{Float64} # stored in reverse order cumsum for easy sorting (first entry corresponds to last Xs)
-    partial_Xs::Vector{Float64} # stored in reverse order cumsum for easy sorting (first entry corresponds to last Xs)
+    partial_XWs::Vector{Float64} # stored in reverse order cumsum for easy sorting (first entry corresponds to last Xs)
     last_i::Int
 end
 
@@ -14,14 +14,15 @@ function RunningCDFEstimator(X::Vector{Float64}, W::Vector{Float64})
     # TODO: combine weights of duplicate entries in cost for efficiency
     perm = sortperm(X, rev=true)
     Xs = X[perm]
-    partial_Xs = cumsum(Xs)
-    Xs = reverse(Xs)
     Ws = W[perm]
     partial_Ws = cumsum(Ws)
+    XWs = Xs .* Ws
+    partial_XWs = cumsum(XWs)
+    Xs = reverse(Xs)
 
     n = length(X)
 
-    return RunningCDFEstimator(Xs, partial_Ws, partial_Xs, n)
+    return RunningCDFEstimator(Xs, partial_Ws, partial_XWs, n)
 end
 
 """
@@ -53,13 +54,13 @@ end
 Tail cost
 """
 function tail_cost(est::RunningCDFEstimator, x)
-    idx = searchsortedlast(est.Xs, x)
+    idx = searchsortedfirst(est.Xs, x)
     if idx < 1
-        return last(est.partial_Xs) / est.last_i
+        return last(est.partial_XWs) / est.last_i
     elseif idx > length(est.Xs)
         return 0.0
     else
-        return (est.partial_Xs[end - idx + 1]) / est.last_i
+        return (est.partial_XWs[end - idx + 1]) / est.last_i
     end
 end
 
@@ -70,17 +71,17 @@ function update!(est::RunningCDFEstimator, x, w)
     if est.last_i == 0
         push!(est.Xs, x)
         push!(est.partial_Ws, w)
-        push!(est.partial_Xs, x)
+        push!(est.partial_XWs, x*w)
     elseif x < first(est.Xs)
         pushfirst!(est.Xs, x)
         push!(est.partial_Ws, last(est.partial_Ws) + w)
-        push!(est.partial_Xs, last(est.partial_Xs) + x)
+        push!(est.partial_XWs, last(est.partial_XWs) + x*w)
     elseif x > last(est.Xs)
         push!(est.Xs, x)
         pushfirst!(est.partial_Ws, 0.0)
         est.partial_Ws = est.partial_Ws .+ w
-        pushfirst!(est.partial_Xs, 0.0)
-        est.partial_Xs = est.partial_Xs .+ x
+        pushfirst!(est.partial_XWs, 0.0)
+        est.partial_XWs = est.partial_XWs .+ x*w
     else
         new_idx = searchsortedlast(est.Xs, x) + 1
         if est.Xs[new_idx-1] == x   # special handle for efficient handling of exactly equal values
@@ -89,10 +90,10 @@ function update!(est::RunningCDFEstimator, x, w)
             splice!(est.Xs, new_idx:new_idx-1, [x])
             w_idx = length(est.partial_Ws) - new_idx + 2
             splice!(est.partial_Ws, w_idx:w_idx-1, [est.partial_Ws[w_idx-1]])
-            splice!(est.partial_Xs, w_idx:w_idx-1, [est.partial_Xs[w_idx-1]])
+            splice!(est.partial_XWs, w_idx:w_idx-1, [est.partial_XWs[w_idx-1]])
         end
         est.partial_Ws[w_idx:end] = est.partial_Ws[w_idx:end] .+ w
-        est.partial_Xs[w_idx:end] = est.partial_Xs[w_idx:end] .+ x
+        est.partial_XWs[w_idx:end] = est.partial_XWs[w_idx:end] .+ x*w
     end
     est.last_i += 1
 end
